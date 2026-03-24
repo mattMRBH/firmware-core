fe9046b1f90af0b81af1cf607ebdd3a6ba656dc2
samuelbles07 <samuelbles07@gmail.com>
2026-03-24 23:32:47 +0500

    add common function get serial number

--- components/airgradient-common/common.cpp
+++ components/airgradient-common/common.cpp
@@ -9,6 +9,10 @@
 
 #include "rtos.h"
 
+#ifndef TEST_HOST
+#include "esp_mac.h"
+#endif
+
 static constexpr uint32_t EXT_WDT_PULSE_MS = 20;
 
 bool ext_watchdog_init(const gpio::Hal &hal, int pin) {
@@ -20,4 +24,22 @@ void ext_watchdog_reset(const gpio::Hal &hal, int pin) {
   hal.set_level(pin, 1);
   RTOS::delay_ms(EXT_WDT_PULSE_MS);
   hal.set_level(pin, 0);
+}
+
+std::string build_serial_number() {
+#ifndef TEST_HOST
+  uint8_t mac_address[6];
+  esp_err_t err = esp_read_mac(mac_address, ESP_MAC_WIFI_STA);
+  if (err != ESP_OK) {
+    printf("Failed to get MAC address (%s)\n", esp_err_to_name(err));
+    return {};
+  }
+
+  char result[13] = {0};
+  snprintf(result, sizeof(result), "%02x%02x%02x%02x%02x%02x", mac_address[0], mac_address[1],
+           mac_address[2], mac_address[3], mac_address[4], mac_address[5]);
+  return std::string(result);
+#else
+  return {};
+#endif
 }
--- components/airgradient-common/include/common.h
+++ components/airgradient-common/include/common.h
@@ -10,11 +10,17 @@
 
 #include "airgradient_gpio.h"
 
+#include <string>
+
 // Configures pin as output and drives it LOW.
 // Returns false if GPIO configuration fails.
 bool ext_watchdog_init(const gpio::Hal &hal, int pin);
 
 // Pulses pin HIGH for a fixed 20 ms then back LOW.
 void ext_watchdog_reset(const gpio::Hal &hal, int pin);
+
+// Builds a 12-character hex serial number from the Wi-Fi STA MAC address.
+// Returns an empty string on failure or when running under TEST_HOST.
+std::string build_serial_number();
 
 #endif // COMMON_H
