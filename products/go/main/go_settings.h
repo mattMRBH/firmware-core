494ddd5d2426cbc7035ffc3b36378ae267998c54
MRBH Engineering <87774892+mattMRBH@users.noreply.github.com>
2026-08-13 14:46:23 -0500

    Add persisted update_interval_seconds to GoSettings

--- products/go/main/go_settings.h
+++ products/go/main/go_settings.h
@@ -1,10 +1,8 @@
 *** Begin Patch
 *** Update File: products/go/main/go_settings.h
 @@
--  // --- Measurement interval ---
--  int measure_interval_seconds = MEASURE_INTERVAL_SECONDS_DEFAULT;
-+  // --- Measurement interval ---
-+  int measure_interval_seconds = MEASURE_INTERVAL_SECONDS_DEFAULT;
+   // --- Measurement interval ---
+   int measure_interval_seconds = MEASURE_INTERVAL_SECONDS_DEFAULT;
 +
 +  // --- Cloud POST cadence (seconds) ---
 +  // Persisted user choice for how frequently the device posts measurements
