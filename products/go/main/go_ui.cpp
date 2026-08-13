d2495e353084a20805e10a11e1c4be685b67a2f3
MRBH Engineering <87774892+mattMRBH@users.noreply.github.com>
2026-08-13 15:19:43 -0500

    UIManager: apply Update Int. choice to GoSettings in apply_to_settings

--- products/go/main/go_ui.cpp
+++ products/go/main/go_ui.cpp
@@ -1,34 +1,17 @@
 *** Begin Patch
 *** Update File: products/go/main/go_ui.cpp
 @@
- uint8_t UIManager::setting_option_count(uint8_t setting_id) const {
-   switch (setting_id) {
-@@
-   case SETTING_MEASURE_INTERVAL:
-     return MEASURE_INTERVAL_COUNT;
-+  case SETTING_UPDATE_INTERVAL:
-+    return UPDATE_INTERVAL_COUNT;
-@@
- }
-@@
- uint8_t UIManager::setting_current_option(uint8_t setting_id) const {
-   switch (setting_id) {
-@@
-   case SETTING_MEASURE_INTERVAL:
-     return _setting_measure_interval;
-+  case SETTING_UPDATE_INTERVAL:
-+    return _setting_update_interval;
-@@
- }
-@@
-   switch (_editing_setting_id) {
-@@
-   case SETTING_MEASURE_INTERVAL:
-     _setting_measure_interval = option_index;
-     break;
-+  case SETTING_UPDATE_INTERVAL:
-+    _setting_update_interval = option_index;
-+    break;
-@@
- }
+   settings.measure_interval_seconds = index_to_seconds(_setting_measure_interval);
++
++  // Map Update interval option index back to seconds.
++  static constexpr int UPDATE_INTERVAL_SECONDS[] = {60, 300, 900, 3600, 10800};
++  static constexpr uint8_t UPDATE_INTERVAL_COUNT = 5;
++
++  auto update_index_to_seconds = [](uint8_t index) -> int {
++    if (index < UPDATE_INTERVAL_COUNT)
++      return UPDATE_INTERVAL_SECONDS[index];
++    return UPDATE_INTERVAL_SECONDS[0];
++  };
++
++  settings.update_interval_seconds = update_index_to_seconds(_setting_update_interval);
 *** End Patch
