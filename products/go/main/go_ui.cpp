7df76a8a1216f3af3efeeacc0acf9e06c08df03b
MRBH Engineering <87774892+mattMRBH@users.noreply.github.com>
2026-08-13 15:22:40 -0500

    UI: expose Update Int. options in SettingsChoice list

--- products/go/main/go_ui.cpp
+++ products/go/main/go_ui.cpp
@@ -1,17 +1,12 @@
 *** Begin Patch
 *** Update File: products/go/main/go_ui.cpp
 @@
-   settings.measure_interval_seconds = index_to_seconds(_setting_measure_interval);
-+
-+  // Map Update interval option index back to seconds.
-+  static constexpr int UPDATE_INTERVAL_SECONDS[] = {60, 300, 900, 3600, 10800};
-+  static constexpr uint8_t UPDATE_INTERVAL_COUNT = 5;
-+
-+  auto update_index_to_seconds = [](uint8_t index) -> int {
-+    if (index < UPDATE_INTERVAL_COUNT)
-+      return UPDATE_INTERVAL_SECONDS[index];
-+    return UPDATE_INTERVAL_SECONDS[0];
-+  };
-+
-+  settings.update_interval_seconds = update_index_to_seconds(_setting_update_interval);
+   switch (_editing_setting_id) {
+@@
+   case SETTING_MEASURE_INTERVAL:
+     options = MEASURE_INTERVAL_OPTIONS;
+     break;
++  case SETTING_UPDATE_INTERVAL:
++    options = UPDATE_INTERVAL_OPTIONS;
++    break;
 *** End Patch
